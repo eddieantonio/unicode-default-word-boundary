3fdc02c10948f7dc0146d03dc1d3e88c8b2715b1
Eddie Antonio Santos <easantos@ualberta.ca>
2019-05-06 09:53:50 -0600

    Minor updates.

--- index.ts
+++ index.ts
@@ -7,10 +7,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,6 +19,11 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
+ // See: https://unicode.org/reports/tr29/#Default_Word_Boundaries
+ // See: https://github.com/eddieantonio/ocreval/blob/master/src/word.c
+ // See: https://github.com/eddieantonio/ocreval/blob/master/libexec/generate_word_break.py
+ // See: https://github.com/eddieantonio/ocreval/blob/master/src/word_break_property.h
 
 export function splitWords(text: string): string[] {
   return [];
