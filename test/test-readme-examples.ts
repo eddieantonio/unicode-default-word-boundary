16587dcdc76eb813d2e297de204dd4c3e1ab3310
Eddie Antonio Santos <easantos@ualberta.ca>
2019-05-15 12:25:49 -0600

    Add tests for README stuff.

---
+++ test/test-readme-examples.ts
@@ -0,0 +1,29 @@
+import test, { Macro } from 'ava';
+import { split } from '../src';
+
+// Macros
+const splitExample: Macro<[string, string[]]> = (t, input, output) => {
+  t.deepEqual(split(input), output);
+};
+splitExample.title = (providedTitle = '', input, _expected) =>
+  `split(\`${input}\`)`;
+
+
+// Tests
+
+// English pangram
+test(splitExample, 'The quick (“brown”) fox can’t jump 32.3 feet, right?', [
+  'The', 'quick', '(', '“', 'brown', '”', ')', 'fox', 'can’t', 'jump', '32.3', 'feet', ',', 'right', '?'
+]);
+
+// Russian pangram
+test(splitExample, `В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!`, [
+  'В', 'чащах', 'юга', 'жил', 'бы', 'цитрус', '?', 'Да', ',', 'но', 'фальшивый', 'экземпляр', '!'
+]);
+
+// TODO: Hebrew example... I don't want to insert RTL text here...
+
+// Plains cree example
+test(splitExample, `ᑕᐻ ᒥᔪ ᑭᓯᑲᐤ ᐊᓄᐦᐨ᙮`, [
+  'ᑕᐻ', 'ᒥᔪ ᑭᓯᑲᐤ', 'ᐊᓄᐦᐨ', '᙮'
+]);
