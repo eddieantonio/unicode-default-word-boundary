46aad74173dbddd42fc50e84468290355ea351bd
Eddie Antonio Santos <easantos@ualberta.ca>
2019-05-16 12:13:02 -0600

    Refactor: macro for test cases where the input is the output.

--- test/test-split-words.ts
+++ test/test-split-words.ts
@@ -21,24 +21,20 @@ test('WB3a', wordBoundaryRule, '\na', ['a']);
 test('WB3b', wordBoundaryRule, 'a\n', ['a']);
 // TODO: test for WB3c
 test('WB3d', wordBoundaryRule, 'a \u2009 b', ['a', 'b'])
-test('WB4 [Extend]', wordBoundaryRule,
-  `pho${COMBINING_HORN}${COMBINING_HOOK_ABOVE}`,
-  [`pho${COMBINING_HORN}${COMBINING_HOOK_ABOVE}`]);
-test('WB4 [Format]', wordBoundaryRule,
-  `Ka${SHY}wen${SHY}non:${SHY}nis`,
-  [`Ka${SHY}wen${SHY}non:${SHY}nis`]);
-test('WB4 [ZWJ]', wordBoundaryRule, `क${VIRAMA}${ZWJ}ष`, ['क्‍ष']);
-test('WB5', wordBoundaryRule, 'aא', ['aא'])
-test('WB6 & WB7', wordBoundaryRule, "ain't", ["ain't"])
-test('WB7', wordBoundaryRule, "ain't", ["ain't"])
-test('WB7a', wordBoundaryRule, `${ALEPH}'`, [`${ALEPH}'`]);
-test('WB7b & WB7c', wordBoundaryRule, `${ALEPH}"${ALEPH}`, [`${ALEPH}"${ALEPH}`]);
-test('WB8', wordBoundaryRule, '42', ['42']);
-test('WB9', wordBoundaryRule, 'A3',  ['A3']);
-test('WB10', wordBoundaryRule, '3a',  ['3a']);
+test('WB4 [Extend]', wordBoundaryRuleIndivisible, `pho${COMBINING_HORN}${COMBINING_HOOK_ABOVE}`);
+test('WB4 [Format]', wordBoundaryRuleIndivisible, `Ka${SHY}wen${SHY}non:${SHY}nis`);
+test('WB4 [ZWJ]', wordBoundaryRuleIndivisible, `क${VIRAMA}${ZWJ}ष`);
+test('WB5', wordBoundaryRuleIndivisible, 'aא')
+test('WB6 & WB7', wordBoundaryRuleIndivisible, "ain't")
+test('WB7', wordBoundaryRuleIndivisible, "ain't")
+test('WB7a', wordBoundaryRuleIndivisible, `${ALEPH}'`);
+test('WB7b & WB7c', wordBoundaryRuleIndivisible, `${ALEPH}"${ALEPH}`);
+test('WB8', wordBoundaryRuleIndivisible, '42');
+test('WB9', wordBoundaryRuleIndivisible, 'A3',);
+test('WB10', wordBoundaryRuleIndivisible, '3a',);
 test('WB11  && WB12', wordBoundaryRule, '3.2 3,456.789',  ['3.2', '3,456.789']);
-test('WB13', wordBoundaryRule, 'エラー',  ['エラー']);
-test('WB13a & WB13b', wordBoundaryRule, 'ᐁ ᓂᐸᐟ',  ['ᐁ ᓂᐸᐟ']);
+test('WB13', wordBoundaryRuleIndivisible, 'エラー',);
+test('WB13a & WB13b', wordBoundaryRuleIndivisible, 'ᐁ ᓂᐸᐟ');
 // TODO: this one is tough because there can be an aribrary amount of RI indicators.
 test.skip('WB15', wordBoundaryRule, '🇨🇦🇰🇭',  ['🇨🇦🇰🇭']);
 test('WB99', wordBoundaryRule, '米饼',  ['米', '饼']);
@@ -46,4 +42,12 @@ test('WB99', wordBoundaryRule, '米饼',  ['米', '饼']);
 /** Macro to test a word boundary rule. */
 function wordBoundaryRule(t: ExecutionContext, input: string, expected: string[]) {
   t.deepEqual(split(input), expected);
+}
+
+/** 
+ * Same as wordBoundaryRule, but the input should not have any internal word
+ * boundaries. i.e., the input is indivisible.
+ */
+function wordBoundaryRuleIndivisible(t: ExecutionContext, input: string) {
+  t.deepEqual(split(input), [input]);
 }
