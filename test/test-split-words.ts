7461868b1ff98ec9af4656354c2b37a65f18dcc0
Eddie Antonio Santos <easantos@ualberta.ca>
2019-05-18 10:06:31 -0600

    Minor, perfective changes.

--- test/test-split-words.ts
+++ test/test-split-words.ts
@@ -3,14 +3,14 @@ import test, { ExecutionContext } from 'ava';
 import {split} from '../src';
 
 ///// Special characters that I don't trust text editors to display properly /////
-/////                       in string literals:                              /////
+/////                 ~~~   in string literals   ~~~                         /////
 // I'm avoiding placing Hebrew and Latin in the same string literal, because
 // VSCode gets VERY confused with bidirectional text ☹️
 const ALEPH = 'א';
 const COMBINING_HOOK_ABOVE = '\u0309'; // phở real
 const COMBINING_HORN = '\u031B'; // phở sure
-const SHY = '\u00AD'; // soft-hypen: a word-break *opporunity*, but NOT a word break!
-const VIRAMA = '\u094D'; // halant in Hindi. Makes the inherent vowel silent.
+const SHY = '\u00AD'; // soft-hyphen: a word-break *opportunity*, but NOT a word break!
+const VIRAMA = '\u094D'; // Halant in Hindi. Makes the inherent vowel silent.
 const ZWJ = '\u200D';
 const EMOJI_PRESENTATION_SELECTOR = '\ufe0f'; // Makes the last thing display as emoji.
 
@@ -32,7 +32,7 @@ test('WB7b & WB7c', wordBoundaryRuleIndivisible, `${ALEPH}"${ALEPH}`);
 test('WB8', wordBoundaryRuleIndivisible, '42');
 test('WB9', wordBoundaryRuleIndivisible, 'A3',);
 test('WB10', wordBoundaryRuleIndivisible, '3a',);
-test('WB11  && WB12', wordBoundaryRule, '3.2 3,456.789',  ['3.2', '3,456.789']);
+test('WB11 & WB12', wordBoundaryRule, '3.2 3,456.789',  ['3.2', '3,456.789']);
 test('WB13', wordBoundaryRuleIndivisible, 'エラー',);
 test('WB13a & WB13b', wordBoundaryRuleIndivisible, 'ᐁ ᓂᐸᐟ');
 test('WB15 & WB16', wordBoundaryRule, '🇨🇦🇰🇭🇽🇽',  ['🇨🇦', '🇰🇭', '🇽🇽']);
